7cbc9f24626f5c3481d7b615d1c7cc3b25b3b7cc
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-10-14 01:38:17 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,87 @@
+import sys
+
+sys.dont_write_bytecode = True
+
+from hokireceh_claimer import base
+from core.token import get_token
+from core.info import get_info
+from core.task import process_do_task
+from core.farm import process_farming
+
+import time
+
+
+class PitchTalk:
+    def __init__(self):
+        # Get file directory
+        self.data_file = base.file_path(file_name="data.txt")
+        self.config_file = base.file_path(file_name="config.json")
+
+        # Initialize line
+        self.line = base.create_line(length=50)
+
+        # Initialize banner
+        self.banner = base.create_banner(game_name="PitchTalk")
+
+        # Get config
+        self.auto_do_task = base.get_config(
+            config_file=self.config_file, config_name="auto-do-task"
+        )
+
+        self.auto_farm = base.get_config(
+            config_file=self.config_file, config_name="auto-farm"
+        )
+
+    def main(self):
+        while True:
+            base.clear_terminal()
+            print(self.banner)
+            data = open(self.data_file, "r").read().splitlines()
+            num_acc = len(data)
+            base.log(self.line)
+            base.log(f"{base.green}Number of accounts: {base.white}{num_acc}")
+
+            for no, data in enumerate(data):
+                base.log(self.line)
+                base.log(f"{base.green}Account number: {base.white}{no+1}/{num_acc}")
+
+                try:
+                    token = get_token(data=data)
+
+                    if token:
+
+                        get_info(token=token)
+
+                        # Do task
+                        if self.auto_do_task:
+                            base.log(f"{base.yellow}Auto Do Task: {base.green}ON")
+                            process_do_task(token=token)
+                        else:
+                            base.log(f"{base.yellow}Auto Do Task: {base.red}OFF")
+
+                        # Farm
+                        if self.auto_farm:
+                            base.log(f"{base.yellow}Auto Farm: {base.green}ON")
+                            process_farming(token=token)
+                        else:
+                            base.log(f"{base.yellow}Auto Farm: {base.red}OFF")
+
+                        get_info(token=token)
+
+                    else:
+                        base.log(f"{base.red}Token not found! Please get new query id")
+                except Exception as e:
+                    base.log(f"{base.red}Error: {base.white}{e}")
+
+            print()
+            wait_time = 60 * 60
+            base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
+            time.sleep(wait_time)
+
+
+if __name__ == "__main__":
+    try:
+        pitchtalk = PitchTalk()
+        pitchtalk.main()
+    except KeyboardInterrupt:
+        sys.exit()
