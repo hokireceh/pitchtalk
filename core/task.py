649792ab96b76611140e8de472708f014f65974e
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-11-07 22:17:28 +0700

    Update task.py

--- core/task.py
+++ core/task.py
@@ -61,43 +61,6 @@ def verify(data, token, proxies=None):
         return None
 
 
-def create_daily(data, token, slug, proof, proxies=None):
-    url = "https://api.pitchtalk.app/v1/api/tasks/create-daily"
-    payload = {"slug": slug, "proof": proof}
-
-    try:
-        response = requests.post(
-            url=url,
-            headers=headers(data, token=token),
-            json=payload,
-            proxies=proxies,
-            timeout=20,
-        )
-        data = response.json()
-
-        return data
-    except:
-        return None
-
-
-def generate_random_links():
-    # Generate random username (between 5 and 20 characters)
-    username_length = random.randint(5, 20)
-    username = "".join(
-        random.choices(string.ascii_letters + string.digits, k=username_length)
-    )
-
-    # Generate random IDs for X and TikTok links
-    x_status_id = random.randint(10**17, 10**18)
-    tiktok_photo_id = random.randint(10**15, 10**18)
-
-    # Construct the X and TikTok links
-    x_link = f"https://x.com/{username}/status/{x_status_id}"
-    tiktok_link = f"https://www.tiktok.com/@{username}/photo/{tiktok_photo_id}"
-
-    return x_link, tiktok_link
-
-
 def process_do_task(data, token, proxies=None):
     task_list = get_task(data=data, token=token, proxies=proxies)
     for task in task_list:
@@ -124,33 +87,6 @@ def process_do_task(data, token, proxies=None):
                 base.log(
                     f"{base.white}{task_name}: {base.yellow}{do_task_status} -> {base.red}{verify_status}"
                 )
-
-    x_link, tiktok_link = generate_random_links()
-
-    daily_tasks = [
-        {
-            "slug": "share-x",
-            "proof": x_link,
-        },
-        {
-            "slug": "share-tiktok",
-            "proof": tiktok_link,
-        },
-    ]
-
-    for daily_task in daily_tasks:
-        slug = daily_task["slug"]
-        proof = daily_task["proof"]
-        do_daily_task = create_daily(
-            data=data, token=token, slug=slug, proof=proof, proxies=proxies
-        )
-        try:
-            daily_task_name = do_daily_task["template"]["title"]
-            daily_task_status = do_daily_task["status"]
-            base.log(f"{base.white}{daily_task_name}: {base.yellow}{daily_task_status}")
-        except:
-            msg = do_daily_task["message"].split(":")[1].strip()
-            base.log(f"{base.white}Daily tasks: {base.red}{msg}")
 
 
 def claim_referral(data, token, proxies=None):
